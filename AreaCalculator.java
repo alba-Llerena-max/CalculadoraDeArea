3f2ad427008ba76bdf2e4c313afddeb9cf9a7e6b
alba-Llerena-max <alballerenafotos8@gmail.com>
2025-02-23 20:43:08 +0100

    AreaCalculator.java

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -3,21 +3,9 @@ public class AreaCalculator {
         System.out.println("Calculadora de área para diferentes figuras.");
         
         // Ejemplo de uso
-        double circle = circleArea(5);
-        double rectangle = rectangleArea(4, 6);
         double triangle = triangleArea(3, 8);
         
-        System.out.println("Área del círculo: " + circle);
-        System.out.println("Área del rectángulo: " + rectangle);
         System.out.println("Área del triángulo: " + triangle);
-    }
-
-    public static double circleArea(double radius) {
-        return Math.PI * radius * radius;
-    }
-    
-    public static double rectangleArea(double width, double height) {
-        return width * height;
     }
     
     public static double triangleArea(double base, double height) {
